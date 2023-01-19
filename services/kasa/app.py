d37cc8f0add2c65b05e265d9a017a7c8ea3896b1
Dan Leonard <dcl525@gmail.com>
2023-01-19 10:35:22 +0000

    Merged PR 1179: Adjust Internal Provider
    Fix internal service provider

--- services/kasa/app.py
+++ services/kasa/app.py
@@ -1,20 +1,24 @@
 from dotenv import load_dotenv
 from framework.abstractions.abstract_request import RequestContextProvider
-from framework.dependency_injection.provider import InternalProvider
+from framework.di.static_provider import InternalProvider
 from framework.serialization.serializer import configure_serializer
-from framework.swagger.quart.swagger import Swagger
 from quart import Quart
+
+from routes.cache import cache_bp
 from routes.devices import devices_bp
 from routes.events import events_bp
 from routes.health import health_bp
 from routes.preset import preset_bp
-from routes.cache import cache_bp
 from routes.region import region_bp
 from routes.scene import scene_bp
 from utils.provider import ContainerProvider
 
 load_dotenv()
+
 app = Quart(__name__)
+
+ContainerProvider.initialize_provider()
+InternalProvider.bind(ContainerProvider.get_service_provider())
 
 
 app.register_blueprint(health_bp)
@@ -32,13 +36,10 @@ async def startup():
         app=app)
 
 
-swag = Swagger(
-    app=app,
-    title='kasa-api')
-swag.configure()
-
-ContainerProvider.initialize_provider()
-InternalProvider.bind(ContainerProvider.get_service_provider())
+# swag = Swagger(
+#     app=app,
+#     title='kasa-api')
+# swag.configure()
 
 
 configure_serializer(app)
--- services/kasa/utils/provider.py
+++ services/kasa/utils/provider.py
@@ -1,20 +1,21 @@
 
-from unicodedata import decimal
 from framework.auth.azure import AzureAd
 from framework.clients.cache_client import CacheClientAsync
+from framework.clients.feature_client import FeatureClientAsync
 from framework.configuration.configuration import Configuration
 from framework.di.service_collection import ServiceCollection
 from framework.di.static_provider import ProviderBase
+from httpx import AsyncClient
 from motor.motor_asyncio import AsyncIOMotorClient
-from framework.clients.feature_client import FeatureClientAsync
 
+from clients.event_client import EventClient
 from clients.identity_client import IdentityClient
 from clients.kasa_client import KasaClient
-from clients.event_client import EventClient
 from data.repositories.kasa_client_response_repository import \
     KasaClientResponseRepository
 from data.repositories.kasa_device_repository import KasaDeviceRepository
-from data.repositories.kasa_device_state_repository import KasaDeviceStateRepository
+from data.repositories.kasa_device_state_repository import \
+    KasaDeviceStateRepository
 from data.repositories.kasa_preset_repository import KasaPresetRepository
 from data.repositories.kasa_region_repository import KasaRegionRepository
 from data.repositories.kasa_scene_category_repository import \
@@ -26,14 +27,13 @@ from providers.kasa_device_provider import KasaDeviceProvider
 from providers.kasa_device_state_provider import KasaDeviceStateProvider
 from services.kasa_client_response_service import KasaClientResponseService
 from services.kasa_device_service import KasaDeviceService
+from services.kasa_device_state_service import KasaDeviceStateService
 from services.kasa_event_service import KasaEventService
 from services.kasa_execution_service import KasaExecutionService
 from services.kasa_preset_service import KasaPresetSevice
 from services.kasa_region_service import KasaRegionService
 from services.kasa_scene_category_service import KasaSceneCategoryService
 from services.kasa_scene_service import KasaSceneService
-from services.kasa_device_state_service import KasaDeviceStateService
-from httpx import AsyncClient
 
 
 def configure_http_client(container):
